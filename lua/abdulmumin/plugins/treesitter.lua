6490bacc085b17f5ce48c7e7b9310a6ba709c541
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-08-01 02:19:23 +0100

    feat(treesitter): add textobjects and update keymaps for incremental selection
    Add `nvim-treesitter-textobjects` as a dependency.
    
    Configure `textobjects` to enable selection, movement, and swapping based on
    tree-sitter nodes.
    
    Update keymaps for incremental selection to provide more intuitive and
    ergonomic options.
    The new keymaps are:
    - `gnn` for initial selection
    - `gns` for incremental node selection
    - `gnc` for incremental scope selection
    - `gnx` for decremental node selection.

--- lua/abdulmumin/plugins/treesitter.lua
+++ lua/abdulmumin/plugins/treesitter.lua
@@ -4,13 +4,14 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-textobjects", -- Add textobjects
 	},
 	config = function()
 		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
-
 		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
+		treesitter.setup({
+			-- enable syntax highlighting
 			highlight = {
 				enable = true,
 			},
@@ -50,10 +51,93 @@ return {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+					init_selection = "gnn",
+					node_incremental = "gns", -- s for "select more"
+					scope_incremental = "gnc", -- c for "class/scope"
+					node_decremental = "gnx", -- x for "shrink/less"
+				},
+			},
+			-- Add textobjects configuration
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+						["ai"] = "@conditional.outer",
+						["ii"] = "@conditional.inner",
+						["a/"] = "@comment.outer",
+						["i/"] = "@comment.inner",
+						["ab"] = "@block.outer",
+						["ib"] = "@block.inner",
+						["as"] = "@statement.outer",
+						["is"] = "@statement.inner",
+						["ad"] = "@call.outer",
+						["id"] = "@call.inner",
+						["am"] = "@call.outer", -- method call
+						["im"] = "@call.inner", -- method call
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]a"] = "@parameter.inner",
+						["]o"] = "@loop.*",
+						["]s"] = "@statement.outer",
+						["]b"] = "@block.outer",
+						["]i"] = "@conditional.outer",
+						["]d"] = "@call.outer",
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+						["]A"] = "@parameter.inner",
+						["]O"] = "@loop.*",
+						["]S"] = "@statement.outer",
+						["]B"] = "@block.outer",
+						["]I"] = "@conditional.outer",
+						["]D"] = "@call.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[a"] = "@parameter.inner",
+						["[o"] = "@loop.*",
+						["[s"] = "@statement.outer",
+						["[b"] = "@block.outer",
+						["[i"] = "@conditional.outer",
+						["[d"] = "@call.outer",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+						["[A"] = "@parameter.inner",
+						["[O"] = "@loop.*",
+						["[S"] = "@statement.outer",
+						["[B"] = "@block.outer",
+						["[I"] = "@conditional.outer",
+						["[D"] = "@call.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
 				},
 			},
 		})
